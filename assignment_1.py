08ed83f0058f8fa02110c1d2e290ea4be2b64308
naman-sharma-git <nishusharma0917@gmail.com>
2025-11-04 22:52:22 +0530

    task 1 & task 2

---
+++ assignment_1.py
@@ -0,0 +1,23 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> num1 = float(input("enter first number: "))
+... num2 = float(input("enter second number: "))
+... 
+... add = num1 + num2
+... sub = num1 - num2
+... mul = num1 * num2
+... div = num1 / num2
+... 
+... print("addition", add)
+... print("subtraction", sub)
+... print("multipplication", mul)
+... print("division", div)
+... 
+... 
+... first_name = input("enter first name: ")
+... last_name =  input("enter last name: ")
+... 
+... full_name = first_name + " " + last_name
+... 
+... print(f"hello, {full_name}! welcome to the python program.")
+SyntaxError: multiple statements found while compiling a single statement
